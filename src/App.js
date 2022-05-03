d30090edc6e4df8f611e693302f998829444d6af
ezequielbonasegla <ebonasegla@hotmail.com>
2022-05-03 14:35:43 -0300

    save

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
+import { BrowserRouter,Routes, Route } from 'react-router-dom';
 import './App.css';
+import Navbar from './components/navegation/Navbar'
+import Inicio from './components/pages/Inicio'
+import AboutUs from './components/pages/AboutUs'
+import Shop from './components/pages/Shop'
+import Cart from './components/pages/Cart'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+
+      <BrowserRouter>
+        <Navbar/>
+        <Routes>
+          <Route path='/' exact component={<Inicio data={data} />}></Route>
+          <Route path='/aboutUs' exact component={<AboutUs />}></Route>
+          <Route path='/shop' exact component={<Shop />}></Route>
+          <Route path='/cart' exact component={<Cart />}></Route>
+        </Routes>
+        </BrowserRouter>
+
     </div>
   );
 }
---
+++ src/components/pages/AboutUs.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const aboutUs = () => {
+    return (
+        <div>
+            <h1>Pagina de Acerca de Nos</h1>
+        </div>
+    )
+}
+
+export default aboutUs
