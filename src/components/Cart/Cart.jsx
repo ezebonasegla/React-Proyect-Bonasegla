437ce77f5d836682f19ccbd79dc59d3d553f1654
ezebonasegla <ebonasegla@gmail.com>
2022-06-20 22:45:31 -0300

    Cart Empty Style

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -3,7 +3,7 @@ import "./Cart.scss";
 import { Link } from 'react-router-dom';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faTrashAlt } from '@fortawesome/free-solid-svg-icons';
-import { faArrowLeft } from '@fortawesome/free-solid-svg-icons';
+import bag from '../assets/bag.png';
 import React from 'react';
 import { FormExample } from '../Form/Form';
 
@@ -15,87 +15,106 @@ export default function Cart() {
     const envioPrice = 200;
 
     return (
-        <div className="cart" >
-            <div className="cart__header">
-                <h1>Carrito</h1>
-            </div>
-            <div className="cart__body">
-                {cartList.length > 0 ?
-                    <section className="py-5">
-                        <div className="container px-4 px-lg-5 my-5">
-                            <div className="row">
-                                <div className="col-lg-12 p-5 bg-white rounded shadow-sm mb-5">
-                                    <div className="table-responsive">
-                                        <table className="table">
-                                            <thead>
-                                                <tr>
-                                                    <th scope="col" className="border-0 bg-light">
-                                                        <div className="p-2 px-3 text-uppercase">Imagen</div>
-                                                    </th>
-                                                    <th scope="col" className="border-0 bg-light">
-                                                        <div className="p-2 px-3 text-uppercase">Product</div>
-                                                    </th>
-                                                    <th scope="col" className="border-0 bg-light">
-                                                        <div className="py-2 text-uppercase">Precio</div>
-                                                    </th>
-                                                    <th scope="col" className="border-0 bg-light">
-                                                        <div className="py-2 text-uppercase">Cantidad</div>
-                                                    </th>
-                                                    <th scope="col" className="border-0 bg-light">
-                                                        <div className="py-2 text-uppercase">Eliminar</div>
-                                                    </th>
-                                                </tr>
-                                            </thead>
-                                            <tbody>
-                                                {cartList.map(item => (
-                                                    <tr key={item.id}>
-                                                        <td className='border-0 align-middle'><img src={item.img} alt={item.name} className="img-fluid rounded shadow-sm" />
-                                                        </td>
-                                                        <td className="border-0 align-middle">{item.name}
-                                                        </td>
-                                                        <td className="border-0 align-middle"><strong>$ {item.price * item.quantity}</strong></td>
-                                                        <td className="border-0 align-middle">{item.quantity}</td>
-                                                        <td className="border-0 align-middle"><button onClick={() => removeFromCart(item.id)}>
-                                                            <FontAwesomeIcon icon={faTrashAlt} className="cart-icon" />
-                                                        </button></td>
+        <div className="view">
+            <div className="cart" >
+                <div className="cart__header">
+                    <h1>Carrito</h1>
+                </div>
+                <div className="cart__body">
+                    {cartList.length > 0 ?
+                        <section className="py-5">
+                            <div className="container px-4 px-lg-5 my-5">
+                                <div className="row">
+                                    <div className="col-lg-12 p-5 bg-white rounded shadow-sm mb-5">
+                                        <div className="table-responsive">
+                                            <table className="table">
+                                                <thead>
+                                                    <tr>
+                                                        <th scope="col" className="border-0 bg-light">
+                                                            <div className="p-2 px-3 text-uppercase">Imagen</div>
+                                                        </th>
+                                                        <th scope="col" className="border-0 bg-light">
+                                                            <div className="p-2 px-3 text-uppercase">Producto</div>
+                                                        </th>
+                                                        <th scope="col" className="border-0 bg-light">
+                                                            <div className="py-2 text-uppercase">Precio</div>
+                                                        </th>
+                                                        <th scope="col" className="border-0 bg-light">
+                                                            <div className="py-2 text-uppercase">Cantidad</div>
+                                                        </th>
+                                                        <th scope="col" className="border-0 bg-light">
+                                                            <div className="py-2 text-uppercase">Eliminar</div>
+                                                        </th>
                                                     </tr>
-                                                ))}
-                                            </tbody>
-                                        </table>
+                                                </thead>
+                                                <tbody>
+                                                    {cartList.map(item => (
+                                                        <tr key={item.id}>
+                                                            <td className='border-0 align-middle'><img src={item.img} alt={item.name} className="img-fluid rounded shadow-sm" />
+                                                            </td>
+                                                            <td className="border-0 align-middle">{item.name}
+                                                            </td>
+                                                            <td className="border-0 align-middle"><strong>$ {item.price * item.quantity}</strong></td>
+                                                            <td className="border-0 align-middle">{item.quantity}</td>
+                                                            <td className="border-0 align-middle"><button onClick={() => removeFromCart(item.id)}>
+                                                                <FontAwesomeIcon icon={faTrashAlt} className="cart-icon" />
+                                                            </button></td>
+                                                        </tr>
+                                                    ))}
+                                                </tbody>
+                                            </table>
+                                        </div>
+
+                                    </div>
+                                </div>
+                                <div className="row py-5 p-4 bg-white rounded shadow-sm">
+                                    <div className="col-lg-12">
+                                        <div className="bg-light rounded-pill px-4 py-3 text-uppercase fw-bold">Resumen de la Compra</div>
+                                        <div className="p-4">
+                                            <p className="mb-4"><em>Gracias por Confiar en BonaDrinks</em></p>
+                                            <ul className="list-unstyled mb-4">
+                                                <li className="d-flex justify-content-between py-3 border-bottom"><strong className="text-muted">Subtotal</strong><strong>$ {totalPrice()}</strong></li>
+                                                <li className="d-flex justify-content-between py-3 border-bottom"><strong className="text-muted">Costos de envio</strong><strong>$ {envioPrice}</strong></li>
+                                                <li className="d-flex justify-content-between py-3 border-bottom"><strong className="text-muted">Total</strong>
+                                                    <h5 className="fw-bold">$ {totalPrice() + envioPrice}</h5>
+                                                </li>
+                                            </ul>
+                                            <FormExample />
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </section>
+                        :
+                        <section className="py-5">
+                            <div className="container px-4 px-lg-5 my-5">
+                                <div class="container-fluid  mt-100">
+                                    <div class="row">
+
+                                        <div class="col-md-12">
+                                            <div class="card-body cart">
+                                                <div class="col-sm-12 empty-cart-cls text-center">
+                                                    <img src={bag} width="130" height="130" class="img-fluid mb-4 mr-3" />
+                                                    <h3><strong>El carrito esta vacio</strong></h3>
+                                                    <h4>Agrega productos para poder comprarlos :)</h4>
+                                                    <Link to={"/shop"}>
+                                                        <a class="btn btn-primary cart-btn-transform m-3" data-abc="true">Ir a la tienda</a>
+                                                    </Link>
+                                                </div>
+                                            </div>
+                                        </div>
+
+
                                     </div>
 
                                 </div>
                             </div>
-                            <div className="row py-5 p-4 bg-white rounded shadow-sm">
-                                <div className="col-lg-12">
-                                    <div className="bg-light rounded-pill px-4 py-3 text-uppercase fw-bold">Resumen de la Compra</div>
-                                    <div className="p-4">
-                                        <p className="mb-4"><em>Gracias por Confiar en BonaDrinks</em></p>
-                                        <ul className="list-unstyled mb-4">
-                                            <li className="d-flex justify-content-between py-3 border-bottom"><strong className="text-muted">Subtotal</strong><strong>$ {totalPrice()}</strong></li>
-                                            <li className="d-flex justify-content-between py-3 border-bottom"><strong className="text-muted">Costos de envio</strong><strong>$ {envioPrice}</strong></li>
-                                            <li className="d-flex justify-content-between py-3 border-bottom"><strong className="text-muted">Total</strong>
-                                                <h5 className="fw-bold">$ {totalPrice() + envioPrice}</h5>
-                                            </li>
-                                        </ul>
-                                        <FormExample />
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </section>
-                    :
-                    <div className="cart__body__empty">
-                        <h2>El Carrito esta Vacio</h2>
-                        <Link to="/shop">
-                            <button className="cart__body__empty__button">
-                                <FontAwesomeIcon icon={faArrowLeft} className="cart-icon" /> Ir a Comprar
-                            </button>
-                        </Link>
-                    </div>
-                }
+                        </section>
+
+                    }
+                </div>
             </div>
-        </div>
+        </div >
     )
 }
 
--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -8,29 +8,33 @@ import "./Form.scss";
 export const FormExample = () => {
     const { register, handleSubmit } = useForm();
     const { placeAnOrder } = useCartContext();
-    const onSubmit = (data) => Swal.fire("Gracias por tu compra", "", "success") && placeAnOrder(data) ;
+    const onSubmit = (data) => Swal.fire("Gracias por tu compra", "", "success") && placeAnOrder(data);
 
     return (
-        <form onSubmit={handleSubmit(onSubmit)}>
-            <h2>Complete sus datos</h2>
-            <label>Nombre</label>
-            <input {...register("firstName", { required: true, maxLength: 20 })} />
-            <label>Apellido</label>
-            <input {...register("lastName", { required: true, pattern: /^[A-Za-z]+$/i })} />
-            <label>Numero de Telefono</label>
-            <input type="number" {...register("phoneNumber", { required: true })} />
-            <label>Mail</label>
-            <input {...register("email", { required: true, pattern: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i })} />
-            <label>Ciudad</label>
-            <select {...register("city")}>
-                <option value="Quilmes">Quilmes</option>
-                <option value="Bernal">Bernal</option>
-            </select>
-            <label>Direccion</label>
-            <input {...register("address", { required: true })} />
+        <div className="checkout">
+            <form onSubmit={handleSubmit(onSubmit)}>
+                <h2 className="section-heading">Complete sus datos</h2>
+                <div className="payment-form">
+                    <label>Nombre</label>
+                    <input {...register("firstName", { required: true, maxLength: 20 })} />
+                    <label>Apellido</label>
+                    <input {...register("lastName", { required: true, pattern: /^[A-Za-z]+$/i })} />
+                    <label>Numero de Telefono</label>
+                    <input type="number" {...register("phoneNumber", { required: true })} />
+                    <label>Mail</label>
+                    <input {...register("email", { required: true, pattern: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i })} />
+                    <label>Ciudad</label>
+                    <select {...register("city")}>
+                        <option value="Quilmes">Quilmes</option>
+                        <option value="Bernal">Bernal</option>
+                    </select>
+                    <label>Direccion</label>
+                    <input {...register("address", { required: true })} />
 
-            <button type="submit" className="btn btn-success rounded-pill py-2 d-md-block" >CONFIRMAR COMPRA</button>
-        </form >
+                    <button type="submit" className="btn btn-success rounded-pill py-2 d-md-block" >CONFIRMAR COMPRA</button>
+                </div>
+            </form >
+        </div>
     );
 }
 
