8e44e50fb1d183e6b63194cd7861ff9075f59fcb
ezebonasegla <ebonasegla@gmail.com>
2022-06-21 21:36:27 -0300

    Add Toastify

--- src/components/Item/Item.jsx
+++ src/components/Item/Item.jsx
@@ -1,21 +1,33 @@
+import React from "react"
 import { Link } from "react-router-dom"
+import { useCartContext } from "../../Context/CartContext"
+import { ToastContainer, toast } from "react-toastify"
+import 'react-toastify/dist/ReactToastify.css'
 import ItemCount from "../ItemCount/ItemCount"
 import "./Item.scss"
-import { useCartContext } from "../../Context/CartContext"
 
 
 function Item({ product }) {
 
-const { addToCart } = useCartContext()
+  const { addToCart } = useCartContext()
 
-function handleInputType(){}
+  function handleInputType() { }
 
-function onAdd(quantity) {
-  alert(`${quantity} ${product.name} agregados al carrito`)
-  addToCart({...product, quantity})
-}
+  function onAdd(quantity) {
+    toast.success(`${quantity} ${product.name} agregados al carrito`, {
+      position: "top-right",
+      autoClose: 4000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+    });
+    addToCart({ ...product, quantity })
+  }
 
   return (
+    <>
     <div className="card-container">
       <div className="card" >
         <Link to={`/detail/${product.id}/${product.name}`} className='link'>
@@ -25,13 +37,14 @@ function onAdd(quantity) {
         <div className="p-info">
           <h4 className="p-price">${product.price}</h4>
           <h4 className="p-size">{product.size}<span> ml</span></h4>
-          </div>
+        </div>
         <br></br>
         <div className="card-button">
           <ItemCount stock={product.stock} initial={1} onAdd={onAdd} handleInputType={handleInputType} />
         </div>
       </div>
     </div>
+    <ToastContainer /></>
   )
 }
 
